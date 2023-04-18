9c39b81cf1f1c1ac86672e808937564906442635
Jaehyeon Park <eiirwp@gmail.com>
2023-04-18 22:08:47 +0900

    add docker

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   reactStrictMode: true,
   pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
 };
